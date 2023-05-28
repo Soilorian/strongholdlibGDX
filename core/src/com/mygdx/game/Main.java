ab696d03570eba81aac80b493c2d83bf3bfa7021
soilorian <mahdiali3804@gmail.com>
2023-05-28 21:07:21 +0330

    finally

--- core/src/com/mygdx/game/Main.java
+++ core/src/com/mygdx/game/Main.java
@@ -1,5 +1,6 @@
-package org.example;
+package com.mygdx.game;
 
+import com.badlogic.gdx.ApplicationAdapter;
 import org.example.control.Controller;
 import org.example.model.exceptions.CoordinatesOutOfMap;
 import org.example.model.exceptions.NotInStoragesException;
@@ -9,7 +10,7 @@ import javax.sound.sampled.UnsupportedAudioFileException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class Main extends ApplicationAdapter {
     public static void main(String[] args) throws ClassNotFoundException, IOException, UnsupportedAudioFileException, LineUnavailableException, CoordinatesOutOfMap, NotInStoragesException {
         Scanner scanner = new Scanner(System.in);
         Controller.run(scanner);
